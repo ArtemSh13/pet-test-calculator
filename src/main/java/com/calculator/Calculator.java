10d68a863dab77368bdcabb65cf1cf50221c2db6
ArtemSh13 <mozg1303@ya.ru>
2022-06-01 20:41:55 +0300

    Add template of Calculator class

---
+++ src/main/java/com/calculator/Calculator.java
@@ -0,0 +1,19 @@
+package com.calculator;
+
+public class Calculator {
+    double getSum(double a, double b) {
+        return 0.0;
+    }
+
+    double getDifference(double a, double b) {
+        return 0.0;
+    }
+
+    double getProduct(double a, double b) {
+        return 0.0;
+    }
+
+    double getQuotient(double a, double b) {
+        return 0.0;
+    }
+}
---
+++ src/test/java/com/calculator/CalculatorTest.java
@@ -0,0 +1,6 @@
+import org.junit.Test;
+
+public class CalculatorTest {
+    @Test
+    public void addition
+}
