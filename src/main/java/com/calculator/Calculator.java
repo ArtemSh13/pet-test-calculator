ef17630910a6606a89c61006f01e720fd6d9932a
ArtemSh13 <mozg1303@ya.ru>
2022-07-04 23:40:39 +0300

    Add implementation of Calculator class methods

--- src/main/java/com/calculator/Calculator.java
+++ src/main/java/com/calculator/Calculator.java
@@ -2,18 +2,21 @@ package com.calculator;
 
 public class Calculator {
     double getSum(double a, double b) {
-        return 0.0;
+        return a + b;
     }
 
     double getDifference(double a, double b) {
-        return 0.0;
+        return a - b;
     }
 
     double getProduct(double a, double b) {
-        return 0.0;
+        return a * b;
     }
 
-    double getQuotient(double a, double b) {
-        return 0.0;
+    double getQuotient(double a, double b) throws Exception {
+        if (b == 0) {
+            throw  new ArithmeticException("Error: division by zero");
+        }
+        return a / b;
     }
 }
